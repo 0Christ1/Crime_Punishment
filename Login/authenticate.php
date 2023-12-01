45d4d0f5ebc2b36fa42f4bb6ed5b6e6284367597
Christ <Christ42@vtb.link>
2023-12-01 01:22:02 -0500

    Login and Mainframe - Christ

---
+++ Login/authenticate.php
@@ -0,0 +1,24 @@
+<?php
+include "connect.php";
+
+// Get the values from the form
+$un = mysqli_real_escape_string($conn, $_POST['uname']);
+$pass = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+$sql = "SELECT * FROM users WHERE username = '$un'";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) === 1){
+    $row = mysqli_fetch_assoc($result);
+    if($row['password'] === $pass){
+        // Password is correct
+        echo '<script language="javascript">alert("Login Successful!");location.href= "../Mainframe/index.html";</script>';
+    } else {
+        // Password is incorrect
+        echo '<script language="javascript">alert("Incorrect Username or Password!");location.href= "./index.html";</script>';
+    }
+} else {
+    // User does not exist
+    echo '<script language="javascript">alert("User does not exist, Please Register!");location.href= "../Registration/index.html";</script>';
+}
+?>
--- js/intro.js
+++
@@ -1,4 +0,0 @@
-const theButton = document.querySelector("button");
-theButton.addEventListener("click", () => {
-  window.location.href = "./php/index.html";
-});
