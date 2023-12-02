dd6a8357e1b96ef84c6994c4e01e86edfcedab11
wyteeee <yw5688@nyu.edu>
2023-12-01 21:31:50 -0500

    index crime wyte

---
+++ Crime/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crime</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container my-5"> 
+        <h2>List of Officers</h2>
+        <a class="btn btn-primary" href="./Crime_add.php" role="button">New Crime</a>
+        <a class="btn btn-primary" href="./Crime_sort.php" role="button">Sort by Crime ID by Ascending</a>
+        <br>
+        <table class="table">
+            <thead> 
+                <tr>
+                    <th>Crime ID</th>
+                    <th>Classification</th>
+                    <th>Date_charged</th>
+                    <th>Status</th>
+                    <th>Hearing date</th>
+                    <th>Appeal_cut_date</th>
+                </tr> 
+            </thead>
+            <tbody>
+                <?php
+                $servname = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "Project3";
+
+                //Database connection
+                $conn = mysqli_connect($servname, $username, $password, $dbname);
+                if(!$conn){
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT * FROM Crime";
+                $result = $conn->query($sql);
+                
+                if (!$result) {
+                    die("Invalid query: " . $conn->error);
+                }
+
+                while($row = $result->fetch_assoc()){
+                    echo "<tr>
+                            <td>{$row['Crime_ID']}</td>
+                            <td>{$row['Classification']}</td>
+                            <td>{$row['Date_charged']}</td>
+                            <td>{$row['Status']}</td>
+                            <td>{$row['Hearing_date']}</td>
+                            <td>{$row['Appeal_cut_date']}</td>
+                            <td>
+                                <a class='btn btn-primary btn-sm' href='./Crime_update.php?id=" . $row['Crime_ID'] . "'>Edit</a>
+                                <a class='btn btn-danger btn-sm' href='./Crime_delete.php?id=" . $row['Crime_ID'] . "'>Delete</a>
+                            </td>
+                          </tr>";
+                }
+                $conn->close();
+                ?>
+            </tbody>
+        </table>    
+    </div>
+</body>
+</html>
