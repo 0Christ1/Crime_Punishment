96f65a44b4b2f660f9f6f1805f930ad58cfb6d43
fiona meng <fionameng@10-18-138-51.dynapool.wireless.nyu.edu>
2023-12-02 11:59:17 -0500

    fiona crime charge

---
+++ Crime_Charge/crime_charge_add.php
@@ -0,0 +1,146 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Initialize variables
+$id = $Crime_id = $Crime_code = $Charge_status = $Fine_amount = $Court_fee = $Amount_paid = $Pay_due_date = "";
+$errorMessage = $successMesssage = "";
+
+//Database credentials 
+$servname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Project3";
+
+//Database connection
+$conn = mysqli_connect($servname, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//Check for form submission
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $id = $_POST["id"];
+    $Crime_id = $_POST["Crime_id"];
+    $Crime_code = $_POST["Crime_Code"];
+    $Charge_status = $_POST["Charge_status"];
+    $Fine_amount = $_POST["Fine_amount"];
+    $Court_fee = $_POST["Court_fee"];
+    $Amount_paid = $_POST["Amount_paid"];
+    $Pay_due_date = $_POST["Pay_due_date"];
+
+    if (empty($id)) {
+        $errorMessage = "Crime Charge ID is required";
+    } else {
+        $stmt = $conn->prepare("INSERT INTO Crime_charges (Charge_ID, Crime_ID, Crime_Code, Charge_status, Fine_amount, Court_fee, Amount_paid, Pay_due_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("iiisiiis", $id, $Crime_id, $Crime_code, $Charge_status, $Fine_amount, $Court_fee, $Amount_paid, $Pay_due_date);
+
+        if ($stmt->execute()) {
+            $successMesssage = "$id is registered successfully!";
+            header("Location: ./index.php");
+            exit;
+        } else {
+            $errorMessage = "Error: " . $stmt->error;
+        }
+
+        $stmt->close();
+    }
+}
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Crime Charge</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container my-5">
+        <h2>New Crime Charge</h2>
+        <?php if (!empty($errorMessage)): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong><?php echo htmlspecialchars($errorMessage); ?></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+        <?php if (!empty($successMesssage)): ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong><?php echo htmlspecialchars($successMesssage); ?></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Charge ID</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="id" value="<?php echo htmlspecialchars($id); ?>">
+                </div>
+            </div>
+        
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Crime ID</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Crime_id" value="<?php echo htmlspecialchars($Crime_id); ?>">
+                </div>
+            </div> 
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Crime Code</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Crime_Code" value="<?php echo htmlspecialchars($Crime_code); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Charge Status</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Charge_status" value="<?php echo htmlspecialchars($Charge_status); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Fine Amount</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Fine_amount" value="<?php echo htmlspecialchars($Fine_amount); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Court Fee</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Court_fee" value="<?php echo htmlspecialchars($Court_fee); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Amount Paid</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Amount_paid" value="<?php echo htmlspecialchars($Amount_paid); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Pay Due Date</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Pay_due_date" value="<?php echo htmlspecialchars($Pay_due_date); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="./index.php" role="button">Cancel</a>
+                </div>
+            </div>
+            
+        </form>
+    </div>
+</body>
+</html>
---
+++ Crime_Charge/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crime Charge</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container my-5"> 
+        <h2>List of Crime Charge</h2>
+        <a class="btn btn-primary" href="./crime_charge_add.php" role="button">New Crime Charge</a>
+        <a class="btn btn-primary" href="./crime_charge_sort.php" role="button">Sort by Crime Charge ID by Ascending</a>
+        <br>
+        <table class="table">
+            <thead> 
+                <tr>
+                    <th>Charge ID</th>
+                    <th>Crime ID</th>
+                    <th>Crime Code</th>
+                    <th>Charge Status</th>
+                    <th>Fine Amount</th>
+                    <th>Court Fee</th>
+                    <th>Amount Paid</th>
+                    <th>Pay Due Date</th>
+                </tr> 
+            </thead>
+            <tbody>
+                <?php
+                $servname = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "Project3";
+
+                //Database connection
+                $conn = mysqli_connect($servname, $username, $password, $dbname);
+                if(!$conn){
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT * FROM Crime_charges";
+                $result = $conn->query($sql);
+                
+                if (!$result) {
+                    die("Invalid query: " . $conn->error);
+                }
+
+                while($row = $result->fetch_assoc()){
+                    echo "<tr>
+                            <td>{$row['Charge_ID']}</td>
+                            <td>{$row['Crime_ID']}</td>
+                            <td>{$row['Crime_Code']}</td>
+                            <td>{$row['Charge_status']}</td>
+                            <td>{$row['Fine_amount']}</td>
+                            <td>{$row['Court_fee']}</td>
+                            <td>{$row['Amount_paid']}</td>
+                            <td>{$row['Pay_due_date']}</td>
+                            <td>
+                                <a class='btn btn-primary btn-sm' href='./crime_charge_update.php?id=" . $row['Charge_ID'] . "'>Edit</a>
+                                <a class='btn btn-danger btn-sm' href='./crime_charge_delete.php?id=" . $row['Charge_ID'] . "'>Delete</a>
+                            </td>
+                          </tr>";
+                }
+                $conn->close();
+                ?>
+            </tbody>
+        </table>    
+    </div>
+</body>
+</html>
