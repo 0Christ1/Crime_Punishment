9872b4ed2381be85097ee3db8f85c72a9478cebb
fiona meng <fionameng@10-18-138-51.dynapool.wireless.nyu.edu>
2023-12-05 09:43:07 -0500

    fiona crime code

--- Mainframe/Crime_Code/Crime_Code_sort.php
+++ Mainframe/Crime_Code/Crime_Code_sort.php
@@ -1,16 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crime Code</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container my-5">
+  <head>
+    <title>Sort Crime Code</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta
+      name="keywords"
+      content="New York Urban Department, NYUPD, Police, Urban Safty"
+    />
+    <meta name="description" content="New York Urban Police Department" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+
+    <link
+      href="../../Styles/global.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <link
+      href="../../Styles/header-agencies.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <link
+      href="../../Styles/homepage-hero.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="../../Styles/index.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="../../Styles/agency-styles.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+  </head>
+  <body id="agencies-index">
+    <div class="agency-header">
+      <div class="upper-header-black">
+        <div class="container">
+          <span class="upper-header-left"
+            ><img src="../../Assets/NYU.png" alt="NYU" class="small-nyc-logo" />
+            <img
+              src="https://www.nyc.gov/assets/home/images/global/upper-header-divider.gif"
+              alt=""
+            /><span class="upper-header-black-title"
+              >New York Urban Police Department</span
+            ></span
+          ><span class="upper-header-padding"></span
+          ><span class="upper-header-right"
+            ><span class="upper-header-three-one-one">911</span
+            ><img
+              src="https://www.nyc.gov/assets/home/images/global/upper-header-divider.gif"
+              alt=""
+            /><span class="upper-header-search"
+              >Visit NYUPD.gov websites</span
+            ></span
+          >
+        </div>
+      </div>
+    </div>
+    <div role="banner" class="main-header">
+      <div class="header-top">
+        <div class="container">
+          <a
+            href=""
+            class="toggle-mobile-side-nav visible-phone"
+            id="nav-open-btn"
+            >Menu</a
+          ><span class="welcome-text hidden-phone agency-header"
+            >New York Urban's Finest</span
+          >
+
+          <div class="agency-logo-wrapper">
+            <a href="#"
+              ><img
+                class="agency-logo"
+                src="../../Assets/NYUPD-Logo.png"
+                alt="NYUPD New York Urban Police Department"
+            /></a>
+          </div>
+          <div class="hidden-phone" id="header-links">
+            <a class="text-size" href="../../Security/logout.php">Log Out</a>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <nav id="nav">
+          <ul>
+            <li class="nav-home">
+              <a href="../index.php"> Home</a>
+            </li>
+            <li><a href="../../Security/redirect.php?page=Crime">Crime</a></li>
+            <li>
+              <a href="../../Security/redirect.php?page=CrimeCode">Crime Code</a>
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=CrimeCharges"
+                >Crime Charges</a
+              >
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=Criminal">Criminal</a>
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=Officers">Officers</a>
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=Sentencing">Sentencing</a>
+            </li>
+            <li><a href="../../Security/redirect.php?page=Appeal">Appeal</a></li>
+            <li>
+              <a href="https://github.com/0Christ1/NYUPD">Repo</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div class="content-img">
+      <div class="content shadow">
+        <div class="container my-5">
         <h2>List of Crime Code</h2>
+        <div class="position">
         <a class="btn btn-primary" href="./Crime_Code_add.php" role="button">New Crime Code</a>
         <a class="btn btn-primary" href="./Crime_Code_sort.php" role="button">Sort by Crime Code by Ascending</a>
+        </div>
         <br>
         <table class="table">
             <thead>
@@ -55,8 +181,10 @@
             </tbody>
         </table>    
     </div>
-</body>
+      </div>
+    </div>
+    <div class="n_footer">(C) 2023 Golden EightPM Corp. v1.0.0</div>
+  </body>
 </html>
-
-
-
+    
+    
--- Mainframe/Crime_Code/Crime_Code_update.php
+++ Mainframe/Crime_Code/Crime_Code_update.php
@@ -59,23 +59,138 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 $conn->close();
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Crime Code</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta
+      name="keywords"
+      content="New York Urban Department, NYUPD, Police, Urban Safty"
+    />
+    <meta name="description" content="New York Urban Police Department" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+
+    <link
+      href="../../Styles/global.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <link
+      href="../../Styles/header-agencies.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <link
+      href="../../Styles/homepage-hero.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="../../Styles/index.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="../../Styles/agency-styles.css"
+      media="screen"
+      rel="stylesheet"
+      type="text/css"
+    />
 </head>
-<body>
-    <div class="container my-5">
-        <h2>Crime Code Details</h2>
-        <?php if (!empty($errorMessage)): ?>
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong><?php echo htmlspecialchars($errorMessage); ?></strong>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        <?php endif; ?>
+<body id="agencies-index">
+<div class="agency-header">
+    <div class="upper-header-black">
+        <div class="container">
+          <span class="upper-header-left"
+            ><img src="../../Assets/NYU.png" alt="NYU" class="small-nyc-logo" />
+            <img
+              src="https://www.nyc.gov/assets/home/images/global/upper-header-divider.gif"
+              alt=""
+            /><span class="upper-header-black-title"
+              >New York Urban Police Department</span
+            ></span
+          ><span class="upper-header-padding"></span
+          ><span class="upper-header-right"
+            ><span class="upper-header-three-one-one">911</span
+            ><img
+              src="https://www.nyc.gov/assets/home/images/global/upper-header-divider.gif"
+              alt=""
+            /><span class="upper-header-search"
+              >Visit NYUPD.gov websites</span
+            ></span
+          >
+        </div>
+      </div>
+    </div>
+    <div role="banner" class="main-header">
+      <div class="header-top">
+        <div class="container">
+            <span class="welcome-text hidden-phone agency-header"
+            >New York Urban's Finest</span
+          >
+
+          <div class="agency-logo-wrapper">
+            <a href="../Mainframe/index.php"
+              ><img
+                class="agency-logo"
+                src="../../Assets/NYUPD-Logo.png"
+                alt="NYUPD New York Urban Police Department"
+            /></a>
+          </div>
+          <div class="hidden-phone" id="header-links">
+            <a class="text-size" href="../../Security/logout.php">Log Out</a>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <nav id="nav">
+          <ul>
+            <li class="nav-home">
+              <a href="../index.php"> Home</a>
+            </li>
+            <li><a href="../../Security/redirect.php?page=Crime">Crime</a></li>
+            <li>
+              <a href="../../Security/redirect.php?page=CrimeCode">Crime Code</a>
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=CrimeCharges"
+                >Crime Charges</a
+              >
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=Criminal">Criminal</a>
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=Officers">Officers</a>
+            </li>
+            <li>
+              <a href="../../Security/redirect.php?page=Sentencing">Sentencing</a>
+            </li>
+            <li><a href="../../Security/redirect.php?page=Appeal">Appeal</a></li>
+            <li>
+              <a href="https://github.com/0Christ1/NYUPD">Repo</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div class="content-img">
+      <div class="content shadow">
+        <div class="container my-5">
+        <h2>Edit Crime Code</h2>
         <?php if (!empty($successMessage)): ?>
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <strong><?php echo htmlspecialchars($successMessage); ?></strong>
@@ -84,10 +199,9 @@ $conn->close();
         <?php endif; ?>
 
         <form method="post">
-            <input type="hidden" name="code" value='<?php echo htmlspecialchars($code); ?>'>
             <div class="row mb-3">
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" placeholder = "Crime Code" name="code" value="<?php echo htmlspecialchars($code); ?>" required>
+                    <input type="text" id="crime code" class="form-control" placeholder = "Crime Code" name="code" value="<?php echo htmlspecialchars($code); ?>" required oninvalid="setCustomValidity('Crime Code is required.')" oninput="setCustomValidity('')">
                 </div>
             </div>
         
@@ -108,5 +222,18 @@ $conn->close();
     </form>
 
     </div>
+      </div>
+    </div>
+    <div class="n_footer">(C) 2023 Golden EightPM Corp. v1.0.0</div>
+    <script>
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+        var input = document.getElementById('crime code');
+        if (!input.value) {
+            input.setCustomValidity('Crime Code is required.');
+        } else {
+            input.setCustomValidity(''); 
+        }
+    });
+  </script>
 </body>
 </html>
